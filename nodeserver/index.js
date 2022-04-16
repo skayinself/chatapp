9bd036e8fee3e18de41f5c274dd3ab5e6f57b7c3
skayinself <newstoday1950@gmail.com>
2022-04-16 09:50:31 +0530

    commit

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,8 +1,8 @@
 // node server which will handle socket io connections 
 
-const io = require("socket.io")(8000, {
+const io = require("socket.io")((process.env.PORT||8000), {
   cors: {
-    origin: "*",
+    origin: "*"
   },
 });
 const users = {};
